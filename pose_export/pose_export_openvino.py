78bed973dba47ae6e2ac6aa95641f199df9d88b8
Mikhail Evstifeev <mievstifieiev1999@gmail.com>
2023-04-21 17:56:47 +0300

    refactor scripts

--- pose_export/pose_export_openvino.py
+++ pose_export/pose_export_openvino.py
@@ -5,14 +5,28 @@ import numpy as np
 import sys
 from pathlib import Path
 import subprocess
+import json
 
 sys.path.append("./engine")
 #import engine.engine3js as engine
 from engine.parse_poses import parse_poses
 
 class PoseEstimator:
+	"""
+	Pose estimator class.
+
+	"""
 
 	def __init__(self, model_dir, model_name, precision):
+		"""
+		Initializes the pose estimator class.
+
+		Args:
+			model_dir (str): Path to the directory containing the model files.
+			model_name (str): Name of the model.
+			precision (str): Precision of the model.
+		"""
+
 		BASE_MODEL_NAME = f"{model_dir}/public/{model_name}/{model_name}"
 		model_path = Path(BASE_MODEL_NAME).with_suffix(".pth")
 		onnx_path = Path(BASE_MODEL_NAME).with_suffix(".onnx")
@@ -25,3 +39,162 @@ class PoseEstimator:
 				f"omz_downloader " f"--name {model_name} " f"--output_dir {model_dir}"
 			)
 			subprocess.run(download_command, shell=True)
+		if not onnx_path.exists():
+			convert_command = (
+				f"omz_converter "
+				f"--name {model_name} "
+				f"--precisions {precision} "
+				f"--download_dir {model_dir} "
+				f"--output_dir {model_dir}"
+			)
+			subprocess.run(convert_command, shell=True)
+		# initialize inference engine
+		ie_core = Core()
+		# read the network and corresponding weights from file
+		model = ie_core.read_model(model=ir_model_path, weights=model_weights_path)
+		# load the model on the CPU (you can use GPU or MYRIAD as well)
+		compiled_model = ie_core.compile_model(model=model, device_name="CPU")
+		self.infer_request = compiled_model.create_infer_request()
+		self.input_tensor_name = model.inputs[0].get_any_name()
+
+		# get input and output names of nodes
+		self.input_layer = compiled_model.input(0)
+		self.output_layers = list(compiled_model.outputs)
+
+		self.body_edges = np.array(
+			[
+				[0, 1],
+				[0, 9], [9, 10], [10, 11],    # neck - r_shoulder - r_elbow - r_wrist
+				[0, 3], [3, 4], [4, 5],       # neck - l_shoulder - l_elbow - l_wrist
+				[1, 15], [15, 16],            # nose - l_eye - l_ear
+				[1, 17], [17, 18],            # nose - r_eye - r_ear
+				[0, 6], [6, 7], [7, 8],       # neck - l_hip - l_knee - l_ankle
+				[0, 12], [12, 13], [13, 14],  # neck - r_hip - r_knee - r_ankle
+			]
+		)
+
+		self.focal_length = -1  # default
+		self.stride = 8
+		self.player = None
+		self.skeleton_set = None
+		self.bone_name = ["neck", "r_shoulder", "r_elbow", "r_wrist", "l_shoulder", "l_elbow", "l_wrist", "l_eye", "l_ear", "r_eye", "r_ear", "nose", "l_hip", "l_knee", "l_ankle", "r_hip", "r_knee", "r_ankle"]
+
+	def estimate(self, video_path : str, output_path : str, smoothing_window = 15) :
+		"""
+		Estimate pose for a given video
+
+		Args:
+			video_path (str): Path to the video file.
+			output_path (str): Path to the output file.
+			smoothing_window (int, optional): Smoothing window. Defaults to 15.
+		"""
+		cap = cv2.VideoCapture(video_path)
+		poses = []
+		i = 0
+		offset = []
+		while cap.isOpened():
+			# Read the next frame
+			ret, frame = cap.read()
+			if not ret:
+				break
+
+			input_image = cv2.resize(frame, (self.input_layer.shape[3], self.input_layer.shape[2]))
+			input_image = input_image.transpose((2, 0, 1))  # change data layout from HWC to CHW
+			input_image = input_image.reshape(self.input_layer.shape)  # reshape to input shape
+			# run inference
+			self.infer_request.infer({self.input_tensor_name: input_image})
+
+			# A set of three inference results is obtained
+			results = {
+				name: self.infer_request.get_tensor(name).data[:]
+				for name in {"features", "heatmaps", "pafs"}
+			}
+			# Get the results
+			results = (results["features"][0], results["heatmaps"][0], results["pafs"][0])
+			poses_3d, _ = parse_poses(results, 1, self.stride, self.focal_length, True)
+			if len(poses_3d) > 0:
+						# From here, you can rotate the 3D point positions using the function "draw_poses",
+						# or you can directly make the correct mapping below to properly display the object image on the screen
+						poses_3d_copy = poses_3d.copy()
+						x = poses_3d_copy[:, 0::4]
+						y = poses_3d_copy[:, 1::4]
+						z = poses_3d_copy[:, 2::4]
+						poses_3d[:, 0::4], poses_3d[:, 1::4], poses_3d[:, 2::4] = (
+							-z + np.ones(poses_3d[:, 2::4].shape) * 200,
+							-y + np.ones(poses_3d[:, 2::4].shape) * 100,
+							-x,
+						)
+
+						poses_3d = poses_3d.reshape(poses_3d.shape[0], 19, -1)[:, :, 0:3]
+						poses_3d = poses_3d.astype(float)
+						if len(offset) == 0:
+							# Find the center of the skeleton
+							center = np.mean(poses_3d, axis=(0, 1))
+
+							# Compute the offset to move the skeleton to the origin
+							offset = -center
+
+						# Apply the offset to all points of the skeleton
+						poses_3d += offset
+
+			for pose_3d in poses_3d:
+				frame_pose = {
+					"frame": i,
+					"pose": {
+						"neck": {"y":pose_3d[0][0], "z":pose_3d[0][1], "x":pose_3d[0][2]},
+						"r_shoulder": {"y":pose_3d[9][0], "z":pose_3d[9][1], "x":pose_3d[9][2]},
+						"r_elbow": {"y":pose_3d[10][0], "z":pose_3d[10][1], "x":pose_3d[10][2]},
+						"r_wrist": {"y":pose_3d[11][0], "z":pose_3d[11][1], "x":pose_3d[11][2]},
+						"l_shoulder": {"y":pose_3d[3][0], "z":pose_3d[3][1], "x":pose_3d[3][2]},
+						"l_elbow": {"y":pose_3d[4][0], "z":pose_3d[4][1], "x":pose_3d[4][2]},
+						"l_wrist": {"y":pose_3d[5][0], "z":pose_3d[5][1], "x":pose_3d[5][2]},
+						"l_eye": {"y":pose_3d[15][0], "z":pose_3d[15][1], "x":pose_3d[15][2]},
+						"l_ear": {"y":pose_3d[16][0], "z":pose_3d[16][1], "x":pose_3d[16][2]},
+						"r_eye": {"y":pose_3d[17][0], "z":pose_3d[17][1], "x":pose_3d[17][2]},
+						"r_ear": {"y":pose_3d[18][0], "z":pose_3d[18][1], "x":pose_3d[18][2]},
+						"nose": {"y":pose_3d[1][0], "z":pose_3d[1][1], "x":pose_3d[1][2]},
+						"l_hip": {"y":pose_3d[6][0], "z":pose_3d[6][1], "x":pose_3d[6][2]},
+						"l_knee": {"y":pose_3d[7][0], "z":pose_3d[7][1], "x":pose_3d[7][2]},
+						"l_ankle": {"y":pose_3d[8][0], "z":pose_3d[8][1], "x":pose_3d[8][2]},
+						"r_hip": {"y":pose_3d[12][0], "z":pose_3d[12][1], "x":pose_3d[12][2]},
+						"r_knee": {"y":pose_3d[13][0], "z":pose_3d[13][1], "x":pose_3d[13][2]},
+						"r_ankle": {"y":pose_3d[14][0], "z":pose_3d[14][1], "x":pose_3d[14][2]}
+					}
+				}
+				poses.append(frame_pose)
+			i += 1
+			if cv2.waitKey(1) == ord('q'):
+				break
+		cap.release()
+		cv2.destroyAllWindows()
+		poses = self.smoothing(poses, smoothing_window)
+		with open(output_path, "w") as f:
+			json.dump(poses, f)
+
+	def smoothing(self, data : list, window_size : int):
+		"""
+			Smoothing the data using convolutional window
+			Args:
+				data (list): The data to be smoothed
+				window_size (int): The size of the window
+			Returns:
+				new_data (list): The smoothed data
+		"""
+		new_data = data.copy()
+		for name in self.bone_name:
+			x_array = np.zeros(len(data))
+			y_array = np.zeros(len(data))
+			z_array = np.zeros(len(data))
+			for i in range(len(data)):
+				x_array[i] = data[i]["pose"][name]["x"]
+				y_array[i] = data[i]["pose"][name]["y"]
+				z_array[i] = data[i]["pose"][name]["z"]
+			window = np.ones(window_size) / window_size
+			x_array = np.convolve(x_array, window, mode='same')
+			y_array = np.convolve(y_array, window, mode='same')
+			z_array = np.convolve(z_array, window, mode='same')
+			for i in range(len(data)):
+				new_data[i]["pose"][name]["x"] = x_array[i]
+				new_data[i]["pose"][name]["y"] = y_array[i]
+				new_data[i]["pose"][name]["z"] = z_array[i]
+		return new_data
--- scripts/set_coordinate.py
+++ scripts/set_coordinate.py
@@ -4,113 +4,113 @@ import bpy
 import json
 import mathutils
 
-def set_bone_loc(rig, bone, location):
-    """Set the bone location to the given location.
+class RigHandler:
+    """Rig Handler class."""
 
-    Args:
-        rig (bpy.types.Object): The rig object.
-        bone (bpy.types.Bone): The bone to set the location.
-        location (mathutils.Vector): The location to set.
-    """
+    def __init__(self, rig_name : str,):
+        """Initializes a RigHandler"""
+        # выбираем объект, который нужно переместить
+        self.rig = bpy.data.objects[rig_name]
 
-    # Получаем матрицу преобразования конечной точкой относительно центра компонент"""
-    base_bone = rig.pose.bones["neck"]
+    base_rig_bones = {
+        #"neck": "neck",
+        #"r_shoulder": "shoulder.R",
+        #"r_elbow": "forearm_tweak.R",
+        "r_wrist": "hand_ik.R",
+        #"l_shoulder": "shoulder.L",
+        #"l_elbow": "forearm_tweak.L",
+        "l_wrist": "hand_ik.L",
+        "l_eye": None,
+        "l_ear": None,
+        #"r_eye": None,
+        #"r_ear": None,
+        #"nose": None,
+        #"l_hip": None,
+        "l_knee": "shin_tweak.L",
+        "l_ankle": "foot_ik.L",
+        #"r_hip": None,
+        "r_knee": "shin_tweak.R",
+        "r_ankle": "foot_ik.R",
+    }
 
-    base_loc, _, _ = (rig.matrix_world @ base_bone.matrix).decompose()
+    def set_bone_loc(self, bone, location):
+        """Set the bone location to the given location.
 
-    location += base_loc
+        Args:
+            bone (bpy.types.Bone): The bone to set the location.
+            location (mathutils.Vector): The location to set.
+        """
 
-    # Получаем матрицу преобразования кости относительно центра координат
-    matrix = rig.matrix_world @ bone.matrix
+        # Получаем матрицу преобразования конечной точкой относительно центра компонент"""
+        base_bone = self.rig.pose.bones["neck"]
 
-    bone_loc, bone_rot, bone_sca = matrix.decompose()
+        base_loc, _, _ = (self.rig.matrix_world @ base_bone.matrix).decompose()
 
-    mat_out = mathutils.Matrix.LocRotScale(location, bone_rot, bone_sca)
+        location += base_loc
 
-    bone.matrix = rig.matrix_world.inverted() @ mat_out
+        # Получаем матрицу преобразования кости относительно центра координат
+        matrix = self.rig.matrix_world @ bone.matrix
 
-    # Обновляем костную систему
-    bpy.context.view_layer.update()
-    bone.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
+        _, bone_rot, bone_sca = matrix.decompose()
+
+        mat_out = mathutils.Matrix.LocRotScale(location, bone_rot, bone_sca)
+
+        bone.matrix = self.rig.matrix_world.inverted() @ mat_out
+
+        # Обновляем костную систему
+        bpy.context.view_layer.update()
+        bone.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
 
 
-def set_bone_rot(rig, bone, parent_bone):
-    """Set the bone rotation to the given rotation.
+    def set_bone_rot(self, bone, parent_bone):
+        """Set the bone rotation to the given rotation.
 
-    Args:
-        rig (bpy.types.Object): The rig object.
-        bone (bpy.types.Bone): The bone to set the rotation.
-        parent_bone (bpy.types.Bone): The parent bone of the bone to set the rotation.
-    """
-    # Получаем матрицу преобразования кости относительно центра объекта
-    matrix = rig.matrix_world @ bone.matrix
-    parent_matrix = rig.matrix_world @ parent_bone.matrix
+        Args:
+            bone (bpy.types.Bone): The bone to set the rotation.
+            parent_bone (bpy.types.Bone): The parent bone of the bone to set the rotation.
+        """
+        # Получаем матрицу преобразования кости относительно центра объекта
+        matrix = self.rig.matrix_world @ bone.matrix
+        parent_matrix = self.rig.matrix_world @ parent_bone.matrix
 
-    bone_loc, bone_rot, bone_sca = matrix.decompose()
-    paremt_bone_loc, parent_bone_rot, parent_bone_sca = parent_matrix.decompose()
+        bone_loc, _, bone_sca = matrix.decompose()
+        _, parent_bone_rot, _ = parent_matrix.decompose()
 
-    mat_out = mathutils.Matrix.LocRotScale(bone_loc, parent_bone_rot, bone_sca)
+        mat_out = mathutils.Matrix.LocRotScale(bone_loc, parent_bone_rot, bone_sca)
 
-    bone.matrix = rig.matrix_world.inverted() @ mat_out
+        bone.matrix = self.rig.matrix_world.inverted() @ mat_out
 
-    # Обновляем костную систему
-    bpy.context.view_layer.update()
-    bone.keyframe_insert(data_path="rotation_quaternion", frame=bpy.context.scene.frame_current)
+        # Обновляем костную систему
+        bpy.context.view_layer.update()
+        bone.keyframe_insert(data_path="rotation_quaternion", frame=bpy.context.scene.frame_current)
 
-def get_hips_loc(l_hip, r_hip):
-    l_hip_x, l_hip_y, l_hip_z = (l_hip["x"] / 100, l_hip["y"] / 100, l_hip["z"] / 100)
-    r_hip_x, r_hip_y, r_hip_z = (r_hip["x"] / 100, r_hip["y"] / 100, r_hip["z"] / 100)
+    def get_hips_loc(l_hip, r_hip):
+        """Get the hips location.
 
-    middle_x = (l_hip_x + r_hip_x) / 2
-    middle_y = (l_hip_y + r_hip_y) / 2
-    middle_z = (l_hip_z + r_hip_z) / 2
-    return mathutils.Vector((middle_x, middle_y, middle_z))
+        Args:
+            l_hip (dict): The left hip.
+            r_hip (dict): The right hip.
 
-base_rig_bones = {
-    #"neck": "neck",
-    #"r_shoulder": "shoulder.R",
-    #"r_elbow": "forearm_tweak.R",
-    "r_wrist": "hand_ik.R",
-    #"l_shoulder": "shoulder.L",
-    #"l_elbow": "forearm_tweak.L",
-    "l_wrist": "hand_ik.L",
-    "l_eye": None,
-    "l_ear": None,
-    #"r_eye": None,
-    #"r_ear": None,
-    #"nose": None,
-    #"l_hip": None,
-    "l_knee": "shin_tweak.L",
-    "l_ankle": "foot_ik.L",
-    #"r_hip": None,
-    "r_knee": "shin_tweak.R",
-    "r_ankle": "foot_ik.R",
-}
+        Returns:
+            mathutils.Vector: The hips location.
+        """
 
-def main():
-    poses = []
-    with open("C:\\Users\\mievst\\Desktop\\masters\\pose_export\\poses.json", "r") as f:
-        poses = json.loads(f.read())
+        l_hip_x, l_hip_y, l_hip_z = (l_hip["x"] / 100, l_hip["y"] / 100, l_hip["z"] / 100)
+        r_hip_x, r_hip_y, r_hip_z = (r_hip["x"] / 100, r_hip["y"] / 100, r_hip["z"] / 100)
 
-    poses.sort(key = lambda frame: frame["frame"])
+        middle_x = (l_hip_x + r_hip_x) / 2
+        middle_y = (l_hip_y + r_hip_y) / 2
+        middle_z = (l_hip_z + r_hip_z) / 2
+        return mathutils.Vector((middle_x, middle_y, middle_z))
 
-    # выбираем объект, который нужно переместить
-    rig = bpy.data.objects["rig"]
-
-    for i in range(0, len(poses), 5):
-        pose = poses[i]
-    #for pose in poses:
-
+    def set_frame(self, pose : dict):
         bpy.context.scene.frame_set(pose["frame"])
 
         #hips_loc = get_hips_loc(pose["pose"]["l_hip"], pose["pose"]["r_hip"])
-
-        #set_bone(rig, rig.pose.bones["torso"], hips_loc)
-
-        for key, val in base_rig_bones.items():
+        for key, val in self.base_rig_bones.items():
             if val == None:
                 continue
-            bone = rig.pose.bones[val]
+            bone = self.rig.pose.bones[val]
 
             x = (pose["pose"][key]["x"] - pose["pose"]["neck"]["x"]) / 100
             y = (pose["pose"][key]["y"] - pose["pose"]["neck"]["y"]) / 100
@@ -118,19 +118,32 @@ def main():
 
             new_location = mathutils.Vector((x, y, z))
 
-            set_bone_loc(rig, bone, new_location)
+            self.set_bone_loc(bone, new_location)
 
-        set_bone_rot(rig, rig.pose.bones["hand_ik.R"], rig.pose.bones["MCH-forearm_ik.R"])
-        set_bone_rot(rig, rig.pose.bones["hand_ik.L"], rig.pose.bones["MCH-forearm_ik.L"])
+        self.set_bone_rot(self.rig.pose.bones["hand_ik.R"], self.rig.pose.bones["MCH-forearm_ik.R"])
+        self.set_bone_rot(self.rig.pose.bones["hand_ik.L"], self.rig.pose.bones["MCH-forearm_ik.L"])
 
-    rig.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
+    def set_frame_list(self, poses : list[dict]):
+        for pose in poses:
+            self.set_frame(pose)
+        self.rig.keyframe_insert(data_path="location", frame=bpy.context.scene.frame_current)
+
+def main():
+    poses = []
+    with open("C:\\Users\\mievst\\Desktop\\masters\\pose_export\\poses.json", "r") as f:
+        poses = json.loads(f.read())
+
+    poses.sort(key = lambda frame: frame["frame"])
+    handler = RigHandler("rig")
+    handler.set_frame_list(poses)
 
 
 def fake_main():
     rig = bpy.data.objects["rig"]
     bone = rig.pose.bones["neck"]
     new_location = Vector((0.006395, 0.377164, 1.02685))
-    set_bone_loc(rig, bone, new_location)
+    handler = RigHandler("rig")
+    handler.set_bone_loc(bone=bone, new_location=new_location)
 
 main()
 #fake_main()
